fff9405839a396b739d986c6fc539e6bb7f90d10
Minatore0712 <buesra.sahin97@web.de>
2021-03-29 15:50:14 +0200

    Add pokemonList Array

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: 0.7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: "Snorlax",
+        height: 2.1,
+        type: "normal"
+    },
+    {
+        name: "Psyduck",
+        height: 0.8,
+        type: "water"
+    }
+];
