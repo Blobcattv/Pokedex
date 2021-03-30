724b9b7608b46962f973e5152845ad3e705a98ef
Minatore0712 <buesra.sahin97@web.de>
2021-03-30 19:53:48 +0200

    Display pokemon order, change font style

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,22 @@ let pokemonList = [
         name: "Psyduck",
         height: 0.8,
         type: ["water"]
+    },
+    {
+        name: "Tynamo",
+        height: 0.2,
+        type: ["electric"]
     }
 ];
 
-pokemonList.forEach((pokemon) => {
-    document.writeln(`${pokemon.name} is ${pokemon.height} meters high. And is of type ${pokemon.type}.`);
-});
+for (let i = 0; i < pokemonList.length; i++) {
+    const pkm = pokemonList[i];
+    let output = `${pkm.name} is ${pkm.height} meters high.`
+    if (pkm.height > 1.0) {
+        output = output + " - Wow! Thats huge.";
+    }
+    if (pkm.height < 0.3) {
+        output = output + " - So tiny!";
+    }
+    document.write(output + "<br>");
+}
