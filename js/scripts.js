8a8f0075c10adb67c73e36cc2b7117eccb5cb63d
Minatore0712 <buesra.sahin97@web.de>
2021-04-06 20:35:38 +0200

    Wrap list in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,52 @@
-let pokemonList = [
-    {
-        name: "Bulbasaur",
-        height: 0.7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: "Snorlax",
-        height: 2.1,
-        type: ["normal"]
-    },
-    {
-        name: "Psyduck",
-        height: 0.8,
-        type: ["water"]
-    },
-    {
-        name: "Tynamo",
-        height: 0.2,
-        type: ["electric"]
-    }
-];
+let pokemonRepository = (function () {
 
-pokemonList.forEach(function(pokemon) {
+
+    let pokemonList = [
+        {
+            name: "Bulbasaur",
+            height: 0.7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: "Snorlax",
+            height: 2.1,
+            type: ["normal"]
+        },
+        {
+            name: "Psyduck",
+            height: 0.8,
+            type: ["water"]
+        },
+        {
+            name: "Tynamo",
+            height: 0.2,
+            type: ["electric"]
+        }
+    ];
+
+    function add(pokemon) {
+        if (typeof pokemon !== "object") {
+            console.log("not a pokemon, please try again");
+            return;
+        }
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+let pokemonList = pokemonRepository.getAll();
+
+pokemonList.forEach(function (pokemon) {
     let output = pokemon.name + ` is ` + pokemon.height + ` meters high.`;
     if (pokemon.height > 1.0) {
         output = output + " - Wow! Thats huge.";
